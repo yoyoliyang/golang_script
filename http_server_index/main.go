544a0969f66518c35af70c61bf43002d581e24a9
yoyoliyang@gmail.com <yoyoliyang@gmail.com>
2020-09-24 13:59:25 +0800

    http web server for directory index, like python http.server

---
+++ http_server_index/main.go
@@ -0,0 +1,30 @@
+// very simple http server for directory
+// 一个非常简单的http方式目录访问脚本
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var path = flag.String("w", "./", "path")
+var port = flag.String("p", "8008", "port")
+var host = flag.String("h", "0.0.0.0", "host")
+
+func main() {
+
+	flag.Parse()
+	h := *host + ":" + *port
+
+	http.HandleFunc("/", handle)
+	fmt.Println("starting http server:", h)
+	log.Fatal(http.ListenAndServe(h, nil))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	hf := http.FileServer(http.Dir(*path))
+	hf.ServeHTTP(w, r)
+	log.Println(r.Host, r.URL.Path)
+}
