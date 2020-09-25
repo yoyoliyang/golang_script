42cf4fdb1019649ca451dfad4dd56ff7724aa087
yoyoliyang@gmail.com <yoyoliyang@gmail.com>
2020-09-25 13:23:33 +0800

    使用args来访问目录

--- http_server_index/main.go
+++ http_server_index/main.go
@@ -7,24 +7,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-var path = flag.String("w", "./", "path")
 var port = flag.String("p", "8008", "port")
 var host = flag.String("h", "0.0.0.0", "host")
 
-func main() {
+var path string
 
+func main() {
 	flag.Parse()
+
+	if flag.Arg(0) == "" {
+		path = "./"
+	} else {
+		path = flag.Arg(0)
+		f, err := os.Open(path)
+		if err != nil {
+			fmt.Println("error opening file: err:", err)
+			return
+		}
+		defer f.Close()
+	}
+
 	h := *host + ":" + *port
 
 	http.HandleFunc("/", handle)
-	fmt.Println("starting http server:", h, *path)
+	fmt.Println("Listening on port:", h, path)
 	log.Fatal(http.ListenAndServe(h, nil))
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	hf := http.FileServer(http.Dir(*path))
+	hf := http.FileServer(http.Dir(path))
 	hf.ServeHTTP(w, r)
 	log.Println(r.RemoteAddr, r.URL.Path)
 }
